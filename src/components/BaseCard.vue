18d90df5a4adfbab7baf8497f7df96ff87d2511e
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-06-17 15:06:04 +0200

    feat(components): BaseCard component

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="baseCard">
+    <slot />
+  </section>
+</template>
+
+<style scoped lang="scss">
+.baseCard {
+  background-color: #011f35;
+  border-radius: 16px;
+  padding: 20px 32px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  overflow: hidden;
+  width: 100%;
+}
+</style>
