eddb6f472d0262cc58577d8660f10ed94a0aeb7c
Kyrylo Novikov <novikovkyrylo@gmail.com>
2026-06-17 15:11:01 +0200

    feat(App): implement global dashboard background and base structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,67 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BaseCard from '@/components/BaseCard.vue'
+import { ref } from 'vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="container">
+    <header>The Magnificent Seven Companies</header>
+
+    <div class="carousel">
+      <BaseCard class=""></BaseCard><BaseCard class=""></BaseCard><BaseCard class=""></BaseCard
+      ><BaseCard class=""></BaseCard><BaseCard class=""></BaseCard><BaseCard class=""></BaseCard
+      ><BaseCard class=""></BaseCard>
+    </div>
+    <div class="wrapper">
+      <BaseCard>Revenue last 3 years</BaseCard
+      ><BaseCard>Revenue Breakdown Magnificent Seven</BaseCard>
+    </div>
+    <div class="wrapper">
+      <BaseCard>Net Income TTM</BaseCard><BaseCard>Gross Margin in % LQ</BaseCard
+      ><BaseCard>Revenue Growth in % YoY </BaseCard>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  color: #ffffff;
+  box-sizing: border-box;
+}
+#app {
+  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011f35 100%);
+  min-height: 100vh;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
+.container {
+  height: 100vh;
+  max-width: 1440px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 32px;
+}
+.carousel {
+  background-color: #023a6233;
+  border-radius: 20px;
+  padding: 24px;
+  gap: 24px;
+  display: flex;
+  flex-direction: row;
+}
+.wrapper {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  gap: 32px;
+}
+header {
+  width: 100%;
+}
+</style>
